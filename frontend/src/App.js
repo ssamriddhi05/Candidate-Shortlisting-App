bcf54340cc18549279ae25fb8c18ceae2039bf9e
ssamriddhi05 <samriddhi5925@gmail.com>
2026-05-10 21:59:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,42 @@
-// import React from "react";
+import React from "react";
 
-// import CandidateForm from "./components/CandidateForm";
-// import CandidateList from "./components/CandidateList";
-// import JobForm from "./components/JobForm";
-
-// function App() {
-//   return (
-//     <div>
-//       <h1 style={{ textAlign: "center" }}>Candidate Shortlisting App</h1>
-
-//       <CandidateForm />
-
-//       <CandidateList />
-
-//       <JobForm />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useState } from "react";
-import CandidateForm from "./CandidateForm";
-import CandidateList from "./CandidateList";
+import CandidateForm from "./components/CandidateForm";
+import CandidateList from "./components/CandidateList";
+import JobForm from "./components/JobForm";
 
 function App() {
-  const [refresh, setRefresh] = useState(false);
-
-  const triggerRefresh = () => {
-    setRefresh(!refresh);
-  };
-
   return (
     <div>
-      <CandidateForm onAdd={triggerRefresh} />
-      <CandidateList refresh={refresh} />
+      <h1 style={{ textAlign: "center" }}>Candidate Shortlisting App</h1>
+
+      <CandidateForm />
+
+      <CandidateList />
+
+      <JobForm />
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState } from "react";
+// import CandidateForm from "./CandidateForm";
+// import CandidateList from "./CandidateList";
+
+// function App() {
+//   const [refresh, setRefresh] = useState(false);
+
+//   const triggerRefresh = () => {
+//     setRefresh(!refresh);
+//   };
+
+//   return (
+//     <div>
+//       <CandidateForm onAdd={triggerRefresh} />
+//       <CandidateList refresh={refresh} />
+//     </div>
+//   );
+// }
+
+// export default App;
