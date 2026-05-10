3ef21e3d3259aa3c96c3174b377a9721e11113fd
ssamriddhi05 <samriddhi5925@gmail.com>
2026-05-10 19:47:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,40 @@
-import React from "react";
+// import React from "react";
 
-import CandidateForm from "./components/CandidateForm";
-import CandidateList from "./components/CandidateList";
-import JobForm from "./components/JobForm";
+// import CandidateForm from "./components/CandidateForm";
+// import CandidateList from "./components/CandidateList";
+// import JobForm from "./components/JobForm";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 style={{ textAlign: "center" }}>Candidate Shortlisting App</h1>
+
+//       <CandidateForm />
+
+//       <CandidateList />
+
+//       <JobForm />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+import CandidateForm from "./CandidateForm";
+import CandidateList from "./CandidateList";
 
 function App() {
+  const [refresh, setRefresh] = useState(false);
+
+  const triggerRefresh = () => {
+    setRefresh(!refresh);
+  };
+
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Candidate Shortlisting App</h1>
-
-      <CandidateForm />
-
-      <CandidateList />
-
-      <JobForm />
+      <CandidateForm onAdd={triggerRefresh} />
+      <CandidateList refresh={refresh} />
     </div>
   );
 }
